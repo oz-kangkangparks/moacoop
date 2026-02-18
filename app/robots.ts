6158b2cbc5328b5a726d47cf7c51d0ad5f94cf5c
KangJinil <kangji0950@gmail.com>
2026-02-18 22:38:09 +0900

    Modify : 네이버 서치어드바이저 등록 위한 수정

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: 'Yeti',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/private/',
+            },
+        ],
         sitemap: 'https://moacoop.co.kr/sitemap.xml',
     };
 }
